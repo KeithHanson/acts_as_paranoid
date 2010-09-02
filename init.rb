1792512a092c29df59703e6295850185b1ebaf0e
pboy <eno@open-lab.org>
2010-09-02 12:31:31 +0200

    BelongsToWithDeletedAssociations is really unnecessary.

--- init.rb
+++ init.rb
@@ -1,9 +1,4 @@
 class << ActiveRecord::Base
-  def belongs_to_with_deleted(association_id, options = {})
-    returning belongs_to_without_deleted(association_id, options) do
-    end
-  end
-  
   def has_many_without_deleted(association_id, options = {}, &extension)
     returning has_many_with_deleted(association_id, options, &extension) do
       if options[:through]
@@ -14,8 +9,8 @@ class << ActiveRecord::Base
     end
   end
   
-  alias_method_chain :belongs_to, :deleted
   alias_method :has_many_with_deleted, :has_many
   alias_method :has_many, :has_many_without_deleted
 end
+
 ActiveRecord::Base.send :include, Caboose::Acts::Paranoid
