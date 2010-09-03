d6ae20a204f2a91b5d7751e5c5ca7de863939964
eno <eno@open-lab.org>
2010-09-03 14:36:06 +0200

    More ACL tests and examples

--- test/acl_test.rb
+++ test/acl_test.rb
@@ -10,21 +10,19 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 # might be one of "public", "private", "friends", "followers", "world"; 
 # if the object does not support a "privacy" attribute this defaults to "private".
 #
-# Such objects can be read:
+# Read access is granted
 #
-# - from the owner, when "private"
-# - from the owner and his friends, when "friends"
-# - from the owner, his friends, and his followers, when "protected"
-# - from any User, when "public"
-# - from any User and the "nil" special user, when "world"
+# - to the owner, when "private"
+# - to the owner and his friends, when "friends"
+# - to the owner, his friends, and his followers, when "protected"
+# - to any User, when "public"
+# - to any User and the "nil" special user, when "world"
+#
+# Write access is granted to the owner only.
+# Delete access is granted to the owner only.
+# Create access is granted to everyone but the "nil" special user.
 #
 
-#
-# Thread local storage:
-#
-# Current.user(new_user) do 
-#   Current.user # new_user
-# end
 
 module Current
   def self.method_missing(key, *args, &block)
@@ -46,18 +44,91 @@ module Current
   end
 end
 
-class ActiveRecord::Base
-  def self.merge_or_conditions(*conditions)
-    segments = []
+module Acl
+  def self.acl?(klass)
+    klass.column_names.include?("owner_id")
+  end
 
-    conditions.each do |condition|
-      unless condition.blank?
-        sql = sanitize_sql(condition)
-        segments << sql unless sql.blank?
-      end
+  def self.privacy?(klass)
+    acl?(klass) && klass.column_names.include?("privacy")
+  end
+
+  def self.for_owner(klass)
+    return unless acl?(klass)
+    return false unless Current.user
+
+    t = klass.table_name
+    [ "#{t}.owner_id=?", Current.user ]
+  end
+  
+  def self.for_friends(klass)
+    return unless privacy?(klass)
+    return unless Current.user
+    
+    t = klass.table_name
+    [ 
+      "#{t}.privacy IN (?) AND #{t}.owner_id IN (SELECT friend_id FROM friendships WHERE friendships.user_id=?)", 
+      %w(friends protected), 
+      Current.user 
+    ]
+  end
+
+  def self.for_followers(klass)
+    return unless privacy?(klass)
+    return unless Current.user
+    
+    t = klass.table_name
+    [ 
+      "#{t}.privacy IN (?) AND #{t}.owner_id IN (SELECT follower_id FROM followships WHERE followships.leader_id=?)", 
+      %w(protected), 
+      Current.user 
+    ]
+  end
+
+  def self.for_public(klass)
+    return unless privacy?(klass)
+    return unless Current.user
+    
+    t = klass.table_name
+    [ "#{t}.privacy='public'" ]
+  end
+
+  def self.for_world(klass)
+    return unless privacy?(klass)
+    
+    t = klass.table_name
+    [ "#{t}.privacy='world'" ]
+  end
+
+  def self.included(klass)
+    klass.dynamic_scope do
+      next nil if Current.user == :root
+
+      conditions = []
+
+      access = :read
+
+      # grant read access
+      #
+      # - to the owner, when "private"
+      # - to the owner and his friends, when "friends"
+      # - to the owner, his friends, and his followers, when "protected"
+      # - to any User, when "public"
+      # - to any User and the "nil" special user, when "world"
+      #
+      
+      conditions << for_owner(klass) << for_friends(klass) << for_followers(klass) << 
+        for_public(klass) << for_world(klass)
+
+      #
+      # merge conditions
+      segments = conditions.map do |condition|
+        klass.send(:sanitize_sql, condition)
+      end.compact
+
+      conditions = "(#{segments.join(') OR (')})" 
+      { :conditions => conditions }
     end
-
-    "(#{segments.join(') OR (')})" unless segments.empty?
   end
 end
 
@@ -66,60 +137,106 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, :class_name => "User"
 end
 
+class Followship < ActiveRecord::Base
+  belongs_to :follower, :class_name => "User"
+  belongs_to :leader, :class_name => "User"
+end
+
 class User < ActiveRecord::Base
   has_many :friendships, :dependent => :destroy
-  has_many :friends, :through => :friendships, :class_name => 'User'
+  has_many :friends, :through => :friendships
+  
+  has_many :leaderships, :class_name => "Followship", :foreign_key => :leader_id
+  has_many :followers, :through => :leaderships
 
+  has_many :followships, :foreign_key => :follower_id
+  has_many :leaders, :through => :followships
+  
   has_many :posts
 end
 
 class Post < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :owner
 
-  dynamic_scope do
-    conditions = []
-    if Current.user
-      conditions.push [ "user_id=?", Current.user ]
-      conditions.push [ 
-        "posts.privacy='friends' AND user_id IN (SELECT friend_id FROM friendships WHERE user_id=?)", Current.user ]
-    end
-    
-    conditions.push "posts.privacy='public'"
-    { :conditions => Post.merge_or_conditions(*conditions) }
-  end
+  include Acl
 
   validates_inclusion_of :privacy, :in => %w(private friends public)
 end
 
 class AclTest < ActiveSupport::TestCase
-  fixtures :users, :friendships, :posts
+  fixtures :users, :friendships, :posts, :followships
   
   def test_fixtures
     assert_equal [users(:friend)], users(:one).friends
     assert_equal [users(:one)], users(:friend).friends
     assert_equal [], users(:other).friends
+    assert_equal [users(:follower)], users(:one).followers
+    assert_equal [users(:one)], users(:follower).leaders
+  end
+
+  POST_NAMES = [ :private, :friends, :protected, :public, :world ]
+
+  def setup
+    setup_post_ids
+  end
+  
+  def setup_post_ids
+    Current.user(:root) do
+      @post_ids = POST_NAMES.inject({}) do |hash, name|
+        hash.update(name => posts(name).id)
+      end
+    end
+  end
+
+  def post_ids(*syms)
+    syms.map { |sym| @post_ids.fetch(sym) }
+  end
+  
+  def assert_user_sees(*expected_posts)
+    expected_post_ids = post_ids(*expected_posts)
+    assert_equal expected_post_ids.sort, Post.all.ids.sort
+
+    expected_post_ids.each do |id|
+      assert_equal id, Post.find(id).id
+      assert_equal id, Post.find_by_id(id).id
+    end
+  end
+  
+  def assert_user_cannot_see(*unexpected_posts)
+    unexpected_post_ids = post_ids(*unexpected_posts)
+    assert_equal [], Post.all.ids & unexpected_post_ids
+  
+    unexpected_post_ids.each do |id|
+      assert_raise(ActiveRecord::RecordNotFound) { Post.find(id) }
+      assert_nil Post.find_by_id(id)
+    end
   end
   
   def test_on_base_class_for_guest
-    assert_equal [3], Post.all.ids
-    assert_raises(ActiveRecord::RecordNotFound) { Post.find(1).nil? }
+    Current.user(nil) do
+      assert_user_sees :world
+      assert_user_cannot_see :public, :protected, :friends, :private
+    end
   end
   
   def test_on_base_class_for_other
     Current.user(users(:other)) do
-      assert_equal [3], Post.all.ids
+      assert_user_sees :public, :world
+      assert_user_cannot_see :protected, :friends, :private
     end
   end
   
   def test_on_base_class_for_friend
     Current.user(users(:friend)) do
-      assert_equal [2,3], Post.all.ids
+      assert_user_sees :world, :public, :protected, :friends
+      assert_user_cannot_see :private
     end
   end
   
   def test_on_base_class_for_me
     Current.user(users(:one)) do
-      assert_equal [1,2,3], Post.all.ids
+      assert_user_sees :world, :public, :protected, :friends, :private
+      assert_user_cannot_see
     end
   end
 end
--- test/schema/acl.rb
+++ test/schema/acl.rb
@@ -9,9 +9,14 @@ ActiveRecord::Schema.define(:version => 1) do
     t.column :friend_id, :integer
   end
 
+  create_table :followships, :force => true do |t|
+    t.column :leader_id, :integer
+    t.column :follower_id, :integer
+  end
+
   create_table :posts, :force => true do |t|
     t.column :text, :string
-    t.column :user_id, :integer
+    t.column :owner_id, :integer
     t.column :privacy, :string
   end
 end
